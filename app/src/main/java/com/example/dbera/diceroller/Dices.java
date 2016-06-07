f1ad08a4712475083bdc0fc8f80f2de7d96294a3
Daniele Berardini <Daniele Berardini>
2016-06-07 17:29:04 +0200

    empty app

---
+++ app/src/main/java/com/example/dbera/diceroller/Dices.java
@@ -0,0 +1,15 @@
+package com.example.dbera.diceroller;
+
+/**
+ * Created by dbera on 07/06/2016.
+ */
+public interface Dices {
+
+    public int d4();
+    public int d6();
+    public int d8();
+    public int d10();
+    public int d12();
+    public int d20();
+    public int d100();
+}
